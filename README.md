b7131b22cb764ac0cb1ecd2936d2136733c3417f
LesterGallagher <LesterGallagher@gmail.com>
2019-04-14 08:56:32 +0200

    initial

---
+++ README.md
@@ -0,0 +1,53 @@
+# chrome-extension-translate
+
+Translate messages in the _locales directory from the default_language to other languages in the _locales directory or locales listed as arguments to the command.
+
+## Usage
+
+```bash
+Usage: chrome-extension-translate <api_key> [locales...]
+
+Options:
+  -V, --version  output the version number
+  -h, --help     output usage information
+```
+
+## Guide
+
+You can create an api key here: [https://console.cloud.google.com/apis/api/translate.googleapis.com/credentials](https://console.cloud.google.com/apis/api/translate.googleapis.com/credentials)
+
+If you already have a messages.json file in the "en" _locales directory [https://developer.chrome.com/apps/i18n-messages](https://developer.chrome.com/apps/i18n-messages), you can translate it by running: 
+
+Define a "default_locale" in the manifest.json file like this:
+
+```json
+{
+    ...
+    "default_locale": "en",
+    ...
+}
+```
+
+In this example the default locale is english.
+
+```bash
+chrome-extension-translate <API_KEY> fr nl fi
+```
+
+In this example we want to generate translations for the languages: French, Dutch and Finnish. After running the command 3 new folders (fr, nl and fi folders) will be created in the _locales folder. The next time you run this command you don't need to specify the locales because it looks in _locales folder for translations:
+
+```bash
+chrome-extension-translate <API_KEY>
+```
+
+This extension only translates messages that do not already exist. For example, if the "helloWorld" message already exists in french, it will not be translated from english. This allows you to easily edit the french translation without it being overwritten each time you run the command. 
+
+You can run this command from any directory containing _locales folder and a manifest.json file with a default_locale entry.
+
+## Example
+
+```bash
+chrome-extension-translate "$API_KEY" ar am bg bn ca cs da de el en es et fa fi fil fr gu he hi hr hu id it ja kn ko lt lv ml mr ms nl no pl ro ru sk sl sr sv sw ta te th tr uk vi
+```
+
+
---
+++ bin/index.js
@@ -0,0 +1,85 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+const fs = require('fs');
+const trash = require('trash');
+const path = require('path');
+const googleTranslateFactory = require('google-translate');
+
+program
+    .version('0.1.0')
+    .command('chrome-extension-translate <api_key> [locales...]')
+    .action(async function (apiKey, extraLocales) {
+        extraLocales = extraLocales || [];
+        const dir = process.cwd();
+        let manifest;
+
+        try {
+            manifest = JSON.parse(fs.readFileSync(path.join(dir, 'manifest.json').toString()));
+        } catch (err) {
+            console.error('Malformed manifest.json file.');
+            process.exit(1);
+        }
+
+        const defaultLocale = manifest.default_locale;
+
+        if (defaultLocale === undefined) {
+            console.error('You have to specify "default_locale" in your manifest.json file.');
+            exit(1);
+        }
+
+        const defaultLocaleMessages = JSON.parse(fs.readFileSync(
+            path.join(dir, '_locales', defaultLocale, 'messages.json')).toString());
+
+        const locales = fs.readdirSync(path.join(dir, '_locales'))
+            .map(dir => path.basename(dir))
+            .concat(extraLocales)
+            .filter(x => x !== defaultLocale);
+
+        const googleTranslate = googleTranslateFactory(apiKey);
+
+        const translations = new Array(locales.length);
+        for (let i = 0; i < translations.length; i++) {
+            translations[i] = {};
+            if (fs.existsSync(path.join(__dirname, '_locales', locale, 'messages.json'))) {
+                const messages = require(path.join(__dirname, '_locales', locale, 'messages.json'));
+                Object.keys(messages).forEach(key => {
+                    translations[i][key] = { message: messages[key] };
+                });
+            }
+            const keys = Object.keys(defaultLocaleMessages).filter(key => key in translations === false);
+            (await translate(keys.map(x =>
+                defaultLocaleMessages[x].message), defaultLocale, locales[i]
+            )).forEach((message, j) => {
+                translations[i][keys[j]] = { message };
+            });
+        }
+
+        const messagesFiles = locales.map(locale => path.join(dir, '_locales', locale, 'messages.json'));
+
+        await trash(messagesFiles);
+
+        for (let i = 0; i < messagesFiles.length; i++) {
+            if ( !fs.existsSync(path.dirname(messagesFiles[i]))) {
+                fs.mkdirSync(path.dirname(messagesFiles[i]));
+            }
+            fs.writeFileSync(messagesFiles[i], JSON.stringify(translations[i], null, 4));
+        }
+
+        console.log('Done! The old messages.json files have been moved to the trash.');
+
+        function translate(strings, source, target) {
+            return new Promise((resolve, reject) => {
+                googleTranslate.translate(strings, source, target, function (err, translations) {
+                    if (err) {
+                        console.error(err);
+                        process.exit(1);
+                    }
+                    resolve(translations.map(x => x.translatedText));
+                });
+            });
+        }
+    });
+
+program.parse(process.argv);
+
